32e8feb6d3128cf636f317addf3ae4020d2d18f8
Sean Long <seanl@aja.com>
2022-05-10 16:38:19 -0700

    colorbox: More work on the Python demos
    Added a second demo which is a Python version of the set_overlay_text
    demo that was introduced with the Go demos. Also cleaned up both demos
    and updated the README.md.

--- python/README.md
+++ python/README.md
@@ -1,13 +1,20 @@
 # Install the ColorBox OpenAPI requirements
 ```
 cd api
-python3 install --disable-pip-version-check --user -r requirements.txt
+python3 -m pip install --disable-pip-version-check --user -r requirements.txt
 cd -
 ```
 
-# Run get_temp
+# Run get_temp example on IP 192.168.1.100 (use control-c to terminate)
 ```
 cd get_temp
-python3 get_temp.py
+python3 main.py --host 192.168.1.100
+cd -
 ```
 
+# Run set_overlay_text example on IP 192.168.1.100 (use control-c to terminate)
+```
+cd set_overlay_text
+python3 main.py --host 192.168.1.100
+cd -
+```
--- python/get_temp/main.py
+++ python/get_temp/main.py
@@ -10,11 +10,7 @@ else:
     print("This script requires Python 3.6 or higher")
     exit(1)
 
-sys.path.append(
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "../api")
-    )
-)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../api")))
 
 import openapi_client
 from openapi_client.api import default_api
@@ -33,12 +29,12 @@ configuration = openapi_client.Configuration(host=hostAndPort)
 # Enter a context with an instance of the API client
 with openapi_client.ApiClient(configuration) as api_client:
     # Create an instance of the API class
-    api_instance = default_api.DefaultApi(api_client)
+    client = default_api.DefaultApi(api_client)
 
     while True:
         try:
-            status = api_instance.get_all_status()
+            status = client.get_all_status()
             print(f"\rdie temp: {status.device_die_temp.value}", end="")
         except ApiException as e:
-            print(f"Exception when calling DefaultApi->get_all_status(): {e}")
+            print(f"Exception when calling client.get_all_status(): {e}")
         time.sleep(1)
