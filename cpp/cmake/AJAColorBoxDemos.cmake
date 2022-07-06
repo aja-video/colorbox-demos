3f7111a7495e63255393a900feae867be348244f
Sean Long <seanl@aja.com>
2022-07-06 14:41:43 -0700

    colorbox: Fix a "SOJI" string that was still around in the cpp demos

--- cpp/cmake/AJAColorBoxDemos.cmake
+++ cpp/cmake/AJAColorBoxDemos.cmake
@@ -1,4 +1,4 @@
-option(AJA_SOJI_DEMOS_COPY_INTO_BIN "Copy the built demos into the cpp/bin directory?" On)
+option(AJA_COLORBOX_DEMOS_COPY_INTO_BIN "Copy the built demos into the cpp/bin directory?" On)
 
 set(COLORBOX_CLIENT_API_TARGET cpp-qt-client-colorbox)
 
