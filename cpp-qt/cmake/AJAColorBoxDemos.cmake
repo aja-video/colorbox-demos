9dc0160da120daf4d96bddb382e69f841eebd60d
Sean Long <seanl@aja.com>
2023-05-04 16:28:32 -0700

    Fix the cmake cpp-qt build for Linux, needed on some systems

--- cpp-qt/cmake/AJAColorBoxDemos.cmake
+++ cpp-qt/cmake/AJAColorBoxDemos.cmake
@@ -22,6 +22,7 @@ elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_definitions(
         -DAJALinux
         -DAJA_LINUX)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     add_definitions(
         -DAJAMac
