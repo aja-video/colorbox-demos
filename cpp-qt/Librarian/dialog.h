b6941357a9bbe41fb21b84b1e5a739b7207027e3
Bill Bowen <bilbo@aja.com>
2024-03-06 13:26:43 -0800

    bring up more informative dialog when upload fails.

--- cpp-qt/Librarian/dialog.h
+++ cpp-qt/Librarian/dialog.h
@@ -45,7 +45,10 @@ public slots:
     void handleUploadFile(QString summary);
     void handleUploadFileError(QString summary, QNetworkReply::NetworkError error_type, QString error_str)  ;
     void handleUploadMultipleFiles(QString summary);
-    void handleUploadMultipleFilesError(QString summary, QNetworkReply::NetworkError error_type, QString error_str)  ;
+    //void handleUploadMultipleFilesError(QString summary, QNetworkReply::NetworkError error_type, QString error_str)  ;
+    void handleUploadMultipleFilesError(OpenAPI::OAIHttpRequestWorker *worker, QNetworkReply::NetworkError error_type, QString error_str);
+    void handleGetLibararyControl(OpenAPI::OAILibraryControl summary);
+
     void handleGetLibrary(QList<OpenAPI::OAILibraryEntry> summary);
     void handleGetLibraryError(QList<OpenAPI::OAILibraryEntry> summary,QNetworkReply::NetworkError error_type, QString error_str);
 
